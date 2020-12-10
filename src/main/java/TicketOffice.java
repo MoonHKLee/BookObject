86e02d48775b4cd8012407d84a603630b6e44229
MoonHKLee <mf311110@gmail.com>
2020-12-10 18:46:54 +0900

    feature/chapter1-book2

--- src/main/java/TicketOffice.java
+++ src/main/java/TicketOffice.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TicketOffice {
     private Long amount;
     private List<Ticket> tickets = new ArrayList<>();
 
-    public TicketOffice(Long amount, List<Ticket> tickets) {
+    public TicketOffice(Long amount, Ticket ... tickets) {
         this.amount = amount;
-        this.tickets = tickets;
+        this.tickets.addAll(Arrays.asList(tickets));
     }
 
     public Ticket getTicket() {
